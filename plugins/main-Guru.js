1bf8d8cc83c41e1e561a2aa278b465e904cd490c
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2024-05-21 16:27:33 +0530

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -8,22 +8,18 @@ let handler = async m => m.reply(`
 
 Group1:
 _____________________
-https://chat.whatsapp.com/KIdMzyvnjju4GIuJhATpm4
+https://chat.whatsapp.com/EOojWFPhi2204G7mEFFKXc
 _______________________
 
 Group2:
 _______________________
-https://chat.whatsapp.com/BhKaFpy9YYRIwtrZUDZMNY
+https://whatsapp.com/channel/0029VaGyfnWBPzjO0MPYmh3v
 _________________________
 
 *DEVELOPERS*/
 1:___________________🔰 *MOD'S* 🔰_
-wa.me/923184070915
-𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -𝘼𝙝𝙢𝙢𝙞
-
-2:__________________🔰 *MOD'S* 🔰_
-wa.me/233533763772
-*ABRAHAM DWAMENA*
+wa.me/918127875972
+𝐅𝖾ᥣ𝗂𝗑 
 _______________________
 𝐋ᶦᵏᵉ  𝐂ᵒᵐᵐᵉⁿᵗ   𝐒ᵃᵛᵉ   𝐒ʰᵃʳᵉ
 💫   🍷   🎊   🍁   🌙   🪄 
@@ -35,10 +31,10 @@ _______________________
 ┊   💛
 ❤️
 *_▬▭▬▭▬▭▬▭▬▭_*
-*╰─ ➤XLICON V2*                    
+*╰─ ➤𝐅𝖾ᥣ𝗂𝗑*                    
                      ☟︎︎︎
  *̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊̊╰─☞︎︎︎TEAM XLICON
-Salman amand & Abraham Dwamena
+𝐅𝖾ᥣ𝗂𝗑 
  ♥️     ✍🏻ㅤ   📩     📤 
  *_ˡᶦᵏᵉ  ᶜᵒᵐᵐᵉⁿᵗ   ˢᵃᵛᵉ    ˢʰᵃʳᵉ_*
 `.trim())
