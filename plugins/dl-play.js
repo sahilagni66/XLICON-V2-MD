56185986ff2ba673c01f6cc0ac6bac9b81f154bb
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2024-11-15 08:14:30 +0530

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -19,7 +19,7 @@ const handler = async (m, {
     conn.GURUPLAY = conn.GURUPLAY ? conn.GURUPLAY : {};
     await conn.reply(m.chat, wait, m);
     const result = await searchAndDownloadMusic(text);
-    const infoText = `🔰──『 *XLICON-V2 PLAYER* 』── 🔰`;
+    const infoText = `▬᳢̅ ⃞⚜️ᩥ᪶──『 *𝐅𝖾ᥣ𝗂𝗑 𝐏ᥣα𝗒𝖾𝗋* 』── ▬᳢̅ ⃞⚜️ᩥ᪶`;
 
 const orderedLinks = result.allLinks.map((link, index) => {
     const sectionNumber = index + 1;
